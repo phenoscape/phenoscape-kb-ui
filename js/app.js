41f81a15a3e5ae20788f294c0fd324942b5f69aa
Jim Balhoff <balhoff@nescent.org>
2015-06-11 19:53:21 -0400

    Downtime alert.

--- js/app.js
+++ js/app.js
@@ -13,19 +13,5 @@ angular.module('pkb', [
 ]).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeController'});
-    $routeProvider.when('/about/phenoscape/kb', {templateUrl: 'partials/about_phenoscape_kb.html', controller: 'AboutPhenoscapeKBController'});
-    $routeProvider.when('/about/phenoscape', {templateUrl: 'partials/about_phenoscape.html', controller: 'AboutPhenoscapeController'});
-    $routeProvider.when('/entity/:term*', {templateUrl: 'partials/entity.html', controller: 'EntityController'});
-    $routeProvider.when('/taxon/:taxon*', {templateUrl: 'partials/taxon.html', controller: 'TaxonController', reloadOnSearch: false});
-    $routeProvider.when('/gene/:gene*', {templateUrl: 'partials/gene.html', controller: 'GeneController', reloadOnSearch: false});
-    $routeProvider.when('/characterstate/:state*', {templateUrl: 'partials/characterstate.html', controller: 'CharacterStateController'});
-	$routeProvider.when('/contents', {templateUrl: 'partials/contents.html', controller: 'ContentsController'});
-	$routeProvider.when('/presence_absence', {templateUrl: 'partials/presence_absence.html', controller: 'PresenceAbsenceController'});
-    $routeProvider.when('/query_characters', {templateUrl: 'partials/query_characters.html', controller: 'QueryCharacterStatesController'});
-    $routeProvider.when('/query_taxa', {templateUrl: 'partials/query_taxa.html', controller: 'QueryTaxaController'});
-    $routeProvider.when('/query_genes', {templateUrl: 'partials/query_genes.html', controller: 'QueryGenesController'});
-    $routeProvider.when('/query_variation_profile', {templateUrl: 'partials/query_variation_profile.html', controller: 'QueryVariationProfileController'});
-    $routeProvider.when('/ontotrace', {templateUrl: 'partials/ontotrace.html', controller: 'OntoTraceController'});
-    $routeProvider.when('/similarity', {templateUrl: 'partials/similarity.html', controller: 'SimilarityController'});
 	$routeProvider.otherwise({redirectTo: '/home'});
 }]);
